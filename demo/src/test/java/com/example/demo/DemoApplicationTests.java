25121de8a047c03d622efd2859842a426e67c3fe
NORI-517 <chikadanoriyuki17@gmail.com>
2023-05-12 10:22:17 +0300

    reset

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package eek.ee.Personal.project;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PersonalProjectApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
